96d3dbef9944f468443ac190fc1b8de2cd209ca9
kadu-ymd <carlosepy@al.insper.edu.br>
2025-09-14 19:08:43 -0300

    exercicio perceptron commit inicial

--- src/ex_1_data/main.py
+++ src/ex_1_data/main.py
@@ -1,4 +1,4 @@
-from utils import *
+from utils.data import *
 import numpy as np
 import matplotlib.pyplot as plt
 
---
+++ src/ex_2_perceptron/utils/data.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class Data:
+    def __init__(self, mu, std, n):
+        self.mu_x, self.mu_y = mu
+        self.std_x, self.std_y = std
+        self.n = n
+
+    def sample_initialize(self) -> tuple[np.ndarray, np.ndarray]:
+        return np.random.normal(self.mu_x, self.std_x, self.n), np.random.normal(self.mu_y, self.std_y, self.n)
+
+class MultiDimensionData:
+    def __init__(self, mu: list, cov: list, n: int):
+        self.mu = np.array(mu)
+        self.cov = np.array(cov)
+        self.n = n
+    
+    def sample_initialize(self):
+        return np.random.multivariate_normal(self.mu, self.cov, self.n)
